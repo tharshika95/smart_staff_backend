adc211dcb845f180867fb43273fadc7e40cecf50
tharshika95 <tharshika.sathasivam.95@gmail.com>
2024-12-08 19:44:15 +0530

    feature (admin): initial application setup

---
+++ src/main/java/com/smart/staff/SmartStaffApplication.java
@@ -0,0 +1,13 @@
+package com.smart.staff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartStaffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartStaffApplication.class, args);
+	}
+
+}
